3dc23e6bf0a2e84b742d2315514b6c072786e8fe
Zhihan Hu <89308955+h-zhh@users.noreply.github.com>
2023-02-21 12:41:10 +0100

    adding a form on homepage to predict

--- app.py
+++ app.py
@@ -20,12 +20,20 @@ def home():
 @app.route("/predict_api", methods=["POST"])
 def predict_api():
     data = request.json["data"]
-    print(data)
-    print(np.array(list(data.values())).reshape(1, -1))
     new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
     output = regmodel.predict(new_data)
-    print(output[0])
     return jsonify(output[0])
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1, -1))
+    output = regmodel.predict(final_input)[0]
+    return render_template(
+        "home.html",
+        prediction_text=f"The price prediction for the house is ${output} million.",
+    )
 
 
 if __name__ == "__main__":
